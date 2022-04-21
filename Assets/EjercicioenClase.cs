ca71ddd8c053570f72c5a87040b868c17f5d8654
SantiagoMartin1 <101400604+SantiagoMartin1@users.noreply.github.com>
2022-04-21 15:51:36 -0300

    Ejercicio en Clase terminado + Explicacion

---
+++ Assets/EjercicioenClase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjercicioenClase : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (num1 != num2 && num1 > num2)
+        {
+            while (num1 >= num2)
+            {
+                Debug.Log(num1);
+                num1--;
+            }
+        }
+
+        else if(num1 < num2)
+        {
+            Debug.Log("Num 2 debe ser menor a Num1 ");
+        }
+        
+        else if (num1 == num2)
+        {
+            Debug.Log("Ambos numeros no pueden ser iguales");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int counter;
+    public int counterMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+    while (counter <= counterMax)
+    {
+
+    Debug.Log("Esto se esta repitiendo" + counter);
+    counter++;
+
+    }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
